678d95774d8599f9233ac30f00526327d52a350f
Harlan Evans <harlanevans5@gmail.com>
2018-12-04 15:53:36 -0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,64 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import List from './List';
+import GroceryForm from './GroceryForm';
+import Footer from './Footer';
 
 class App extends Component {
+
+  state = { groceries: [], filter: 'All' }
+
+  setFilter = (filter) => {
+    this.setState({ filter })
+  }
+
+  getUniqId = () => {
+    //NOTE We are just using this as a helper function for id's since we aren't using a db yet
+    return Math.floor((1 + Math.random()) * 0x10000)
+      .toString(16)
+      .substring(1);
+  }
+
+  addItem = (name) => {
+    const { groceries } = this.state;
+    const grocery = { name, id: this.getUniqId(), complete: false }
+    this.setState({ groceries: [grocery, ...groceries] });
+  }
+
+  handleClick = (id) => {
+    const { groceries } = this.state;
+    this.setState({
+      groceries: groceries.map(grocery => {
+        if (grocery.id === id) {
+          return {
+            ...grocery,
+            complete: !grocery.complete
+          }
+        }
+        return grocery
+      })
+    })
+  }
+
+  visibleItems = () => {
+    const { groceries, filter } = this.state;
+    switch(filter) {
+      case 'Active':
+        return groceries.filter( t => !t.complete )
+      case 'Complete':
+        return groceries.filter( t=> t.complete )
+      default:
+        return groceries;
+    }
+  }
+  
   render() {
+    const { groceries, filter } = this.state;
+
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <GroceryForm addItem={this.addItem} />
+        <List name="Grocery List" items={this.visibleItems()} groceryClick={this.handleClick}/>
+        <Footer filter={filter} setFilter={this.setFilter} />
       </div>
     );
   }
